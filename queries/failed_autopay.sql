c423ce455594268e9c415b066290e3227d00228d
Hamna Tameez <123413075+htameez@users.noreply.github.com>
2026-05-06 05:19:32 -0500

    Save SQL query for failed autopay attempts in the last 24 hours

--- queries/failed_autopay.sql
+++ queries/failed_autopay.sql
@@ -1,15 +1,3 @@
-SELECT
-    ca.account_id,
-    ca.customer_name,
-    ca.email,
-    ca.phone,
-    aa.attempted_at,
-    aa.amount,
-    aa.failure_reason
-FROM
-    customer_accounts ca
-JOIN
-    autopay_attempts aa ON ca.account_id = aa.account_id
-WHERE
-    aa.status = 'failed'
-    AND aa.attempted_at >= NOW() - INTERVAL '1 day';
+SELECT * 
+FROM autopay_attempts 
+WHERE attempted_at >= NOW() - INTERVAL '1 day' AND status = 'failed';
