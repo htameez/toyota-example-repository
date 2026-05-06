7337519272eb6a383d5fc7d130e76a43e2bd518d
Hamna Tameez <123413075+htameez@users.noreply.github.com>
2026-05-06 05:25:46 -0500

    Add SQL query for failed autopay attempts

--- queries/failed_autopay.sql
+++ queries/failed_autopay.sql
@@ -1,14 +1,4 @@
-SELECT 
-    ca.account_id,
-    ca.customer_name,
-    ca.email,
-    ca.phone,
-    aa.attempted_at,
-    aa.failure_reason
-FROM 
-    customer_accounts ca
-JOIN 
-    autopay_attempts aa ON ca.account_id = aa.account_id
-WHERE 
-    aa.status = 'failed' 
-    AND aa.attempted_at >= NOW() - INTERVAL '24 HOURS';
+SELECT ca.account_id, ca.customer_name, ca.email, ca.phone, aa.attempted_at, aa.failure_reason
+FROM customer_accounts ca
+JOIN autopay_attempts aa ON ca.account_id = aa.account_id
+WHERE aa.status = 'failed' AND aa.attempted_at >= NOW() - INTERVAL '24 HOURS';
